029e7541cefb61c620e1627490f4f3256d9edddc
SudoTech <sudotech.channel@gmail.com>
2017-05-12 09:18:15 -0500

    Update CPIO.java

--- CPIO.java
+++ CPIO.java
@@ -1,56 +1,64 @@
-/*
+/**
  * The CPIO class aims to make GPIO easily accessible with Java on the Next Thing Co CHIP computer
  * The full repository along with README are available at https://github.com/Sudo-Tech/CPIO
  * @author SudoTech (https://www.youtube.com/sudotech)
- * @version 0.1
+ * @version 0.2
  */
 
 public class CPIO {
+	
+	private ProcessBuilder sys;
+	private int pin;
+	private String command;
+	
+	public CPIO(String pin, String state) {
+		pin = pin(pin);
+		command = 
+		sys = new ProcessBuilder(command);
+		Process p = pb.start();
+		p.waitFor();
+	}
 	/**
-	 * Tells system to initialize GPIO pin. Default direction is set to in (read).
-	 * @param pin Pin to initialize
+	 * Reads current pin direction as read (in) or write (out).
+	 * @return current direction (in / out)
 	 */
-	public static void export(String pin) {
-		
+	public String dir() {
+		return "out";
 	}
 	/**
 	 * Sets pin direction to read (in) or write (out).
-	 * @param pin Pin to set
 	 * @param dir Direction to set (in / out)
 	 */
-	public static void set(String pin, String dir) {
+	public void dir(String dir) {
 		
 	}
 	/**
 	 * Reads value of pin as powered/neutral (1) or ground (0). If pin is set to write (0) or not initialized, returns -1.
-	 * @param pin Pin to read
 	 * @return Value of pin (-1 / 0 / 1)
 	 */
-	public static int read(String pin) {
+	public int read() {
 		return -1;
 	}
 	/**
 	 * Writes value to pin as powered (1) or off (0). Returns set value or -1 if pin is set to read or not initialized.
-	 * @param pin Pin to set
 	 * @param val Value to  set (0 / 1)
 	 * @return Set value (-1 / 0 / 1)
 	 */
-	public static int write(String pin, int val) {
+	public int write(int val) {
 		return -1;
 	}
 	/**
-	 * Tells system to unexport the pin.
-	 * @param pin Pin to unexport
+	 * Tells system to unexport the pin and deletes object.
 	 */
-	public static void unexport(String pin) {
+	public void del() {
 		
 	}
 	/**
-	 * Returns current state of read (in) or write (out). Returns none if pin not initialized.
-	 * @param pin Pin to test
-	 * @return Current state (in / out / none)
+	 * Calculates system pin number from friendly string
+	 * @param pin String that refers to pin (XIO-P0 - XIO-P7)
+	 * @return Int that refers to pin for system
 	 */
-	public static String state(String pin) {
-		return "none";
+	private int pin(String pin) {
+		return 1013 + pin.substring(pin.length - 1);
 	}
 }
